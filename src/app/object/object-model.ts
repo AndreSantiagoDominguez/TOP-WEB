65909b3e253e5e6e26bc2fee411ae2b6d43157ac
AndreSantiagoDominguez <233393@ids.upchiapas.edu.mx>
2024-10-02 19:29:00 -0600

    add: se agrego la carpeta servicios para object

--- src/app/object/object-model.ts
+++ src/app/object/object-model.ts
@@ -1,2 +1,11 @@
 export interface ObjectModel {
+   
+        id: number; 
+        name: string; 
+        description: string; 
+        rarity: string; // Rareza (común, raro, épico, legendario)
+        value: number; // Valor en créditos para intercambiar
+        videoGameId: number; // ID del videojuego al que pertenece
+        userId: number; // ID del usuario propietario
+      
 }
