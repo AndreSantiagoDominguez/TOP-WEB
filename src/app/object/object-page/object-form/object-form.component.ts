0adc61716be61b79991a379da19d3be7e5c2340f
AndreSantiagoDominguez <233393@ids.upchiapas.edu.mx>
2024-10-02 20:22:56 -0600

    add: Se creo objectForm y objectCard, para crear y mostrar los objetos.

---
+++ src/app/object/object-page/object-form/object-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-object-form',
+  templateUrl: './object-form.component.html',
+  styleUrl: './object-form.component.css'
+})
+export class ObjectFormComponent {
+
+}
--- src/app/object/object.module.ts
+++ src/app/object/object.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ObjectPageComponent } from './object-page/object-page.component';
+import { ObjectFormComponent } from './object-page/object-form/object-form.component';
+import { ObjectCardComponent } from './object-page/object-card/object-card.component';
 
 
 
 @NgModule({
   declarations: [
-    ObjectPageComponent
+    ObjectPageComponent,
+    ObjectFormComponent,
+    ObjectCardComponent
   ],
   imports: [
     CommonModule
